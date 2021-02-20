82607bff4a5f41a1e8154594c2575b31ed505901
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-02-20 14:34:18 -0500

    commit tree 1cbc2d39b2c48b2c6ef3f039c1566c260455fff7

--- color-clock/index.js
+++ color-clock/index.js
@@ -9,8 +9,6 @@ function displayTime() {
       clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
       clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   }, 1000)
-
-
 }
 
 window.onload = displayTime()
